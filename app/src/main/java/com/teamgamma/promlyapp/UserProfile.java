051cdeb97ef845ec68ede2ba55b9fa6d15292e0e
Shane Curtis <curtisappdevelopment@gmail.com>
2022-04-07 19:57:26 -0400

    Implemented View Profile Screen, including elements to be shown when viewing own profile

---
+++ app/src/main/java/com/teamgamma/promlyapp/UserProfile.java
@@ -0,0 +1,16 @@
+package com.teamgamma.promlyapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+        //Todo: Change image of multifunctional button
+        //Todo: Create settings pages?
+
+    }
+}
