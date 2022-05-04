b3947cdae08cf0973e77cf38239615d8e62c4c93
1pete3 <skowronekpeter103@gmail.com>
2022-05-04 03:26:33 -0400

    Created the explore page where the user can select different items like 1by2day or hustle

--- app/src/main/java/com/teamgamma/promlyapp/MainActivity.java
+++ app/src/main/java/com/teamgamma/promlyapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.teamgamma.promlyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Button m1by2day = findViewById(R.id.button1By2Day);
+        m1by2day.setOnClickListener(view -> {
+            // Switch MainActivity.class with 1by2day class so that when it is pressed it goes to 1by2day
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        Button mHustle = findViewById(R.id.buttonHustle);
+        mHustle.setOnClickListener(view -> {
+            // Switch MainActivity.class with hustle class so that when it is pressed it goes to hustle
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
