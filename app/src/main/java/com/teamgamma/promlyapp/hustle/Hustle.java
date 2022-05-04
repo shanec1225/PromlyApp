2fb9373875bfba4f2ac68fc55788036b3302d7a8
Shane Curtis <curtisappdevelopment@gmail.com>
2022-05-04 14:11:19 -0400

    Fixed some fonts and constraint issues

--- app/src/main/java/com/teamgamma/promlyapp/hustle/Hustle.java
+++ app/src/main/java/com/teamgamma/promlyapp/hustle/Hustle.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,6 +18,15 @@ public class Hustle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hustle);
+
+        // Main Serve button on top
+        ImageView serveButton = findViewById(R.id.serveButton);
+        serveButton.setOnClickListener(view -> {
+            // Switch to serve screen
+            Intent segueToServe = new Intent(view.getContext(), Serve.class);
+            startActivity(segueToServe);
+            overridePendingTransition(R.anim.right_in, R.anim.left_out);
+        });
 
         // "more" button above serve section
         TextView moreServe = findViewById(R.id.moreServeButton);
